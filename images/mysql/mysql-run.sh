48db7bd6d3fedc81ae3d737e3af15dd8a890508a
Pirate Dunbar <yarr@piratedunbar.com>
2016-02-09 17:14:35 -0500

    Fix import script to glob through multiple files instead of just one

--- images/mysql/mysql-run.sh
+++ images/mysql/mysql-run.sh
@@ -3,6 +3,8 @@ set -e
 
 # Script to workaround docker-machine/boot2docker OSX host volume issues: https://github.com/docker-library/mysql/issues/99
 
+mysql=( mysql --protocol=socket -uroot )
+
 echo '* Working around permission errors locally by making sure that "mysql" uses the same uid and gid as the host volume'
 TARGET_UID=$(stat -c "%u" /var/lib/mysql)
 echo '-- Setting mysql user to use uid '$TARGET_UID
@@ -13,4 +15,9 @@ groupmod -o -g $TARGET_GID mysql || true
 echo
 echo '* Starting MySQL'
 chown -R mysql:root /var/run/mysqld/
+
+for f in ./data/dumps/*.sql; do
+    echo "$0: importing dump file $f"; "${mysql[@]}" < "$f"; echo ;;
+done
+
 /entrypoint.sh mysqld --user=mysql --console
